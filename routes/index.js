e70f8667c590281c51c696c5d18ed93226b51430
bakerz <de1719@163.com>
2015-08-28 14:59:23 +0800

    fix #3

--- routes/index.js
+++ routes/index.js
@@ -11,20 +11,28 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+	console.log(req.session);
 	Article.find(function(err, doc) {
 		res.render('index', { 
 			title: '主页' ,
+			user: req.session.user,
+			info: req.flash('info').toString,
+			success: req.flash('success').toString(),
+			error: req.flash('error').toString(),
 			datas: doc
 		});
 	});
 });
 
 router.get('/reg', function(req, res, next) {
-	res.render('register', { title: '注册' });
+	res.render('register', { 
+		title: '注册',
+		user: req.session.user
+	});
 });
 
 router.post('/reg', function(req, res, next) {
-	var name = req.body.name,
+	var username = req.body.username,
 		password = req.body.password,
 		passwordRepeat = req.body.passwordRepeat;
 
@@ -35,7 +43,7 @@ router.post('/reg', function(req, res, next) {
 	}
 
 	//检查用户名是否已经存在
-	User.findOne({name:name}, function(err, user) {
+	User.findOne({username:username}, function(err, user) {
 		if(err) {
 			console.log('error', err);
 			return res.redirect('/reg');
@@ -52,7 +60,7 @@ router.post('/reg', function(req, res, next) {
 			md5password = md5.update(password).digest('hex');
 
 		var newUser = new User({
-			name: name,
+			username: username,
 			password: md5password,
 			email: req.body.email
 		});
@@ -65,11 +73,7 @@ router.post('/reg', function(req, res, next) {
 			console.log('success', '注册成功！');
 			newUser.password = null;
 			delete newUser.password;
-			console.log("+++sessoin.user+++"+req.session.user);
-			console.log("---" + newUser);
 			req.session.user = newUser;
-			console.log("+++" + newUser);
-			console.log("+++sessoin.user+++"+req.session.user);
 			return res.redirect('/');
 		});
 	});
@@ -78,35 +82,47 @@ router.post('/reg', function(req, res, next) {
 router.get('/login', function(req, res, next) {
 	User.find(function(err, doc) {
 		res.render('login', { 
-			title: '登录' ,
+			title: '登录',
+			user: req.session.user,
 			datas: doc
 		});
 	});
 });
 
 router.post('/login', function(req, res, next) {
-	var name = req.body.name,
+	var username = req.body.username,
 		password = req.body.password;
 
-	User.findOne({name:name}, function(err, user) {
+	User.findOne({username:username}, function(err, user) {
 		if(err) {
 			console.log("----err: " + err);
 			return next(err);
 		}
 		if(!user) {
-			req.flash('info', '用户不存在');
+			req.flash('error', '用户不存在');
+			console.log('用户不存在！');
 			return res.redirect('/login');
 		}
-		if(user.password !== password) {
+		//对密码进行md5加密
+		var md5 = crypto.createHash('md5'),
+			md5password = md5.update(password).digest('hex');
+		if(user.password !== md5password) {
 			req.flash('error', '密码错误');
+			console.log('密码错误');
 			return res.redirect('/login');	
 		}
+		user.password = null;
+		delete user.password;
+		req.session.user = user;
 		return res.redirect('/');
 	});
 });
 
 router.get('/post', function(req, res, next) {
-	res.render('post', { title: '发表' });
+	res.render('post', { 
+		title: '发表',
+		user: req.session.user
+	});
 });
 
 router.post('/post', function(req, res, next) {
@@ -127,6 +143,8 @@ router.post('/post', function(req, res, next) {
 });
 
 router.get('/logout', function(req, res, next) {
+	req.session.user = null;
+	req.flash('success','退出登录成功！');
 	res.redirect('/');
 });
 
